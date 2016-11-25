f3a3cddad0bbba01c96c5bcbabaad44cde7b903d
Fangdun Cai <cfddream@gmail.com>
2016-11-25 21:44:26 +0800

    test: improve

--- test.js
+++ test.js
@@ -1,6 +1,81 @@
 import test from 'ava'
-import fn from './'
+import MemoryProvider from './'
 
-test('title', t => {
-  t.is(fn('unicorns'), 'unicorns & rainbows')
+const sleep = n => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, n)
+  })
+}
+
+test('should return an undefined session', t => {
+  const memory = new MemoryProvider()
+
+  const sess = memory.get('233')
+
+  t.is(sess, undefined)
+})
+
+test('should save a session', t => {
+  const memory = new MemoryProvider()
+
+  const sess = {
+    cookie: {}
+  }
+
+  memory.set('233', sess, 1000)
+
+  const sess2 = memory.get('233')
+
+  t.deepEqual(sess, sess2)
+})
+
+test('should save a session again', t => {
+  const memory = new MemoryProvider()
+
+  const sess = {
+    cookie: {}
+  }
+
+  memory.set('233', sess, 1000)
+  memory.set('233', sess, 1000)
+
+  const sess2 = memory.get('233')
+
+  t.deepEqual(sess, sess2)
+})
+
+test('should return a undefined session when expires', t => {
+  const memory = new MemoryProvider()
+
+  const sess = {
+    cookie: {
+      expires: 100
+    }
+  }
+
+  memory.set('233', sess, 100)
+
+  const sess2 = memory.get('233')
+
+  t.is(sess2, undefined)
+})
+
+test('should auto delete when expires', async t => {
+  const memory = new MemoryProvider()
+
+  const sess = {
+    cookie: {
+      expires: 1000
+    }
+  }
+
+  memory.set('233', sess, 1000)
+
+  await sleep(1500)
+
+  const sess2 = memory.get('233')
+
+  t.is(sess2, undefined)
 })
